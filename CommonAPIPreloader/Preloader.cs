5ea28a3287babfc8dee99ef084b4940081a0d6ae
kremnev8 <kremnev8super@yandex.ru>
2021-08-11 18:29:47 +0300

    Separate projects

--- CommonAPIPreloader/Preloader.cs
+++ CommonAPIPreloader/Preloader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BepInEx.Logging;
 using Mono.Cecil;
@@ -25,14 +26,24 @@ namespace CommonAPI
             ModuleDefinition module = assembly.MainModule;
             TypeDefinition entityData = module.Types.First (t => t.FullName == "EntityData");
             TypeDefinition prefabDesc = module.Types.First (t => t.FullName == "PrefabDesc");
+            
+            TypeDefinition recipeType = module.Types.First (t => t.FullName == "ERecipeType");
 
-            bool flag = entityData == null || prefabDesc == null;
+            bool flag = entityData == null || prefabDesc == null || recipeType == null;
             if (flag)
             {
                 logSource.LogInfo("Preloader patching failed!");
                 return;
             }
 
+            //FieldDefinition field = new FieldDefinition("customType", FieldAttributes.Public, module.ImportReference(typeof(int)));
+            //recipeProto.Fields.Add(field);
+            //var ca_2 = new CustomAttribute(assembly.MainModule.ImportReference(typeof(NonSerializedAttribute).GetConstructor(new Type[] {})));
+            //field.CustomAttributes.Add(ca_2);
+            
+            var enumCustomValue = new FieldDefinition("Custom", FieldAttributes.Static | FieldAttributes.Literal | FieldAttributes.Public | FieldAttributes.HasDefault, recipeType) { Constant = 20 };
+            recipeType.Fields.Add(enumCustomValue);
+            
             entityData.Fields.Add(new FieldDefinition("customId", FieldAttributes.Public, module.ImportReference(typeof(int))));
             entityData.Fields.Add(new FieldDefinition("customType", FieldAttributes.Public, module.ImportReference(typeof(int))));
             
--- CommonAPIPreloader/Properties/AssemblyInfo.cs
+++ CommonAPIPreloader/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Preloader")]
+[assembly: AssemblyTitle("CommonAPIPreloader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Preloader")]
+[assembly: AssemblyProduct("CommonAPIPreloader")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
